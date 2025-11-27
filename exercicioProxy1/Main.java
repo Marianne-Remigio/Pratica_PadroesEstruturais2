41bb9c6ea7d39fd9abad4682b22ee56135b0753f
Marianne-Remigio <mariannelimaremigio7@gmail.com>
2025-11-27 00:29:35 -0300

    Create Main.java

---
+++ exercicioProxy1/Main.java
@@ -0,0 +1,31 @@
+package org.example.exercicioProxy1;
+
+/* TODO:  BIBLIOTECA DIGITAL
+    O sistema disponibiliza o acesso a livros digitais, porém esse acesso está aberto para qualquer usuário.
+    É necessário algumas adequações para que o acesso aos conteúdos dependa do tipo de usuário, onde:
+    .
+    1 - Usuários comuns: podem apenas ler o título de um livro;
+        Exibir mensagem: "Assine o plano Premium para acessar o conteúdo completo."
+    2 - Usuários premium: podem ler o conteúdo completo.
+    ...
+    ...
+    Aplique o padrão de Projeto Proxy neste contexto para controle de acesso.
+
+ */
+public class Main {
+
+    public static void main(String[] args) {
+
+        LivroDigitalProxy livroComum = new LivroDigitalProxy(
+            "Java para Iniciantes", "Conteúdo completo do livro..."
+        , false);
+
+        livroComum.ler();
+
+        LivroDigitalProxy livroPremium = new LivroDigitalProxy(
+            "Java para Iniciantes", "Conteúdo completo do livro..."
+            , true);
+
+        livroPremium.ler();
+    }
+}
